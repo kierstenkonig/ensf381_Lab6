80fba8499b096a5d94c1d73a6b0933cbf985f182
GerardoGdL <gerardo.garciadeleon@ucalgary.ca>
2025-03-07 12:34:15 -0700

    Exercise 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  let currentYear = new Date().getFullYear();
+  let isLoggedIn = true;
+  return(
+    <div>
+      <h1>ENSF 381: FUll Stack Web Development</h1>
+      <p>React Components</p>
+      <p>The current year is: {currentYear}</p>
+      <p>{isLoggedIn ? "Welcome Back" : "Please log in" }</p>
+    </div> 
   );
 }
 
