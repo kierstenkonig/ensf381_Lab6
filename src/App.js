c2f58786eb23a551771d5c6d6ec78762addf14f0
kierstenkonig <kiersten.konig@ucalgary.ca>
2025-03-07 12:47:29 -0700

    fixed your typo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   let isLoggedIn = true;
   return(
     <div>
-      <h1>ENSF 381: FUll Stack Web Development</h1>
+      <h1>ENSF 381: Full Stack Web Development</h1>
       <p>React Components</p>
       <p>The current year is: {currentYear}</p>
       <p>{isLoggedIn ? "Welcome Back" : "Please log in" }</p>
